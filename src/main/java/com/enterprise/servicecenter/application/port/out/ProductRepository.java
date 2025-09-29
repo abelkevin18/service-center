19293db4f5d4c5c52cc7bba141f6bbb934e70901
Abel Nunez <abelkevin13@gmail.com>
2025-09-29 06:12:20 -0500

    feat: search products

--- src/main/java/com/enterprise/servicecenter/application/port/out/ProductRepository.java
+++ src/main/java/com/enterprise/servicecenter/application/port/out/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.enterprise.servicecenter.application.port.out;
 
 import com.enterprise.servicecenter.application.model.Product;
+import java.util.List;
 
 public interface ProductRepository {
     void save(Product product);
     Product findById(String id);
+    List<Product> findAll(String input, int pageNumber, int pageSize);
 }
